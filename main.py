37fc7ec0e4b60d070cfd7182a5fd67974936feb2
Mykhaylo Zhyhan <mz5157e@gre.ac.uk>
2026-02-14 22:11:46 +0000

    game using django

--- main.py
+++ main.py
@@ -1,11 +1,43 @@
+import json
+
 import init_django_orm  # noqa: F401
+from django.db.models import QuerySet
 
-from db.models import Race, Skill, Player, Guild
-
-
-def main() -> None:
-    pass
+from db.models import Race, Skill, Guild, Player
 
 
-if __name__ == "__main__":
-    main()
+def main() -> QuerySet:
+    with open("players.json", "r", encoding="utf-8") as file_name:
+        players_data = json.load(file_name)
+    for nickname, data in players_data.items():
+        race_data = data["race"]
+        race, _ = Race.objects.get_or_create(
+            name=race_data["name"],
+            defaults={"description": race_data.get("description", "")},
+        )
+
+        for race_obj in race_data.get("skills", []):
+            Skill.objects.get_or_create(
+                name=race_obj["name"],
+                defaults={"bonus": race_obj.get("bonus", ""), "race": race},
+            )
+
+        guild_obj = None
+        guild_data = data.get("guild")
+        if guild_data is not None:
+            guild_obj, _ = Guild.objects.get_or_create(
+                name=guild_data["name"],
+                defaults={"description": guild_data.get("description")},
+            )
+
+        Player.objects.get_or_create(
+            nickname=nickname,
+            defaults={
+                "email": data["email"],
+                "bio": data["bio"],
+                "race": race,
+                "guild": guild_obj,
+            },
+        )
+
+    return Player.objects.all()
